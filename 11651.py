983b2b7360c0906c412f7cae6f91e7f6d13e9bda
cho8833 <chowsp1009@gmail.com>
2025-09-02 17:20:41 +0900

    11651

--- 11651.py
+++ 11651.py
@@ -1,31 +1,15 @@
-class Node:
-    def __init__(self, value):
-        self.value = value
-        self.left = None
-        self.right = None
+import sys
 
-class BST:
-    def __init__(self, root):
-        self.root = Node(root)
-    def insert(self, value):
-        self.root = self._insert_value(self.root, value)
-        return self.root is not None
-    def _insert_value(self, node, value):
-        if node is None:
-            node = Node(value)
-            return
-        if node.value > value:
-            self._insert_value(node.right, value)
-        else:
-            self._insert_value(node.left, value)
+data = []
+for _ in range(200001):
+    data.append([])
+for _ in range(int(sys.stdin.readline())):
+    n1, n2 = map(int,sys.stdin.readline().split(' '))
+    data[n2].append(n1)
 
-data = {}
-for _ in range(int(input())):
-    n1, n2 = input().split(' ')
-    if n2 in data:
-        data[n2].insert(int(n1))
-    else:
-        data[n2] = BST(int(n1))
-    
-for y in data.keys():
-    print(y)
+for i in range(-100000, 100001, 1):
+    if len(data[i]) != 0:
+        d = sorted(data[i])
+
+        for o in d:
+            print(o,i)
