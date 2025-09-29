97305fbefc143d49ce7015788afc015f8df4e57e
cho8833 <chowsp1009@gmail.com>
2025-09-29 21:42:17 +0900

    9372

---
+++ tree/9372.py
@@ -0,0 +1,15 @@
+import sys
+case = int(input())
+ns = []
+for _ in range(case):
+    n, m = map(int, input().split())
+    ns.append(n)
+    data = dict()
+    visited = dict()
+    for _ in range(m):
+        sys.stdin.readline()
+
+for n in ns:
+    print(n-1)
+        
+    
