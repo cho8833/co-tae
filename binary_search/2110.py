37dfd581197fc7902320eccb385a166471762703
cho8833 <chowsp1009@gmail.com>
2025-10-16 20:00:04 +0900

    bs - 2110

--- binary_search/2110.py
+++ binary_search/2110.py
@@ -6,3 +6,26 @@ for _ in range(n):
     house.append(int(sys.stdin.readline()))
 
 
+house.sort()
+
+answer = 0
+
+start = 1
+end = house[-1] - house[0] + 1
+
+while start + 1 < end:
+    mid = (start + end) // 2
+    
+    current = house[0]
+    count = 1
+    for i in range(1, len(house)):
+        if house[i] >= current + mid:
+            count += 1
+            current = house[i]
+    
+    if count < c:
+        end = mid
+    else:
+        start = mid
+
+print(start)
