4bcb1165ddabb3788874c096bce3363b4d2e71ff
cho8833 <chowsp1009@gmail.com>
2025-11-20 14:48:35 +0900

    bisect - 2143

---
+++ binary_search/2143.py
@@ -0,0 +1,45 @@
+import bisect
+
+T = int(input())
+n = int(input())
+A = list(map(int , input().split()))
+m = int(input())
+B = list(map(int, input().split()))
+
+answer = 0
+
+aSub = dict()
+for i in range(n):
+    pSum = 0
+    for j in range(i, n):
+        pSum += A[j]
+        if pSum not in aSub:
+            aSub[pSum] = 1
+        else:
+            aSub[pSum] += 1
+
+bSub = dict()
+for i in range(m):
+    pSum = 0
+    for j in range(i, m):
+        pSum += B[j]
+        if pSum not in bSub:
+            bSub[pSum] = 1
+        else:
+            bSub[pSum] += 1
+
+bSums = sorted(bSub.keys())
+
+for a in aSub.keys():
+    
+    target = T - a
+
+    bi = bisect.bisect_left(bSums, target)
+
+    for i in range(bi, len(bSums)):
+        if bSums[i] == target:
+            answer += bSub[bSums[i]] * aSub[a]
+        else:
+            break
+
+print(answer)
